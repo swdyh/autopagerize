d384f54fb7b1e77bdac13194bac53c9b17429a50
swdyh <youhei@gmail.com>
2008-05-04 18:59:25 +0900

    ver 0.0.3

--- autopagerize.user.js
+++ autopagerize.user.js
@@ -6,7 +6,7 @@
 // ==/UserScript==
 //
 // auther:  youhei
-// version: 0.0.1 (2007.1.13 04:08:40)
+// version: 0.0.3 (2007.1.17 00:33:36)
 //
 // this script based on
 // GoogleAutoPager(http://la.ma.la/blog/diary_200506231749.htm) and
@@ -17,20 +17,57 @@
 // http://www.gnu.org/copyleft/gpl.html
 //
 (function() {
+    var HTML_NAMESPACE = 'http://www.w3.org/1999/xhtml'
+
+    var SITEINFO_IMPORT_URLS = [
+        // 'http://swdyh.infogami.com/autopagerize',
+    ]
+    var SITEINFO = [
+        {
+            url: 'http://www.google.*/search*',
+            nextLink: 'id("navbar")/table/tbody/tr/td[last()]/a',
+            insertBefore: 'id("navbar")',
+            pageElement: '//div[2]',
+            remainHeight: 800
+        },
+        {
+            url:          'http://del.icio.us/*',
+            nextLink:     '//a[@accesskey="e"]',
+            insertBefore: 'id("bottom")',
+            pageElement:  'id("main")',
+            remainHeight: 1200
+        },
+        {
+            url:          'http://search.yahoo.co.jp/search*',
+            nextLink:     'id("yschpg")/p/big[last()]/a',
+            insertBefore: 'id("yschpg")',
+            pageElement:  'id("yschweb")',
+            remainHeight: 800
+        },
+        {
+            url:          'http://search.auctions.yahoo.co.jp/*',
+            nextLink:     '//td[@align="right" and @width="1%"]/small/b[last()]/a',
+            insertBefore: '//table[position()=11]',
+            pageElement:  '//table[position()=7]',
+            remainHeight: 1200
+        }
+    ]
+
     var AutoPager = function(info, state) {
-        this.pageNum   = 1
-        this.info       = info
-        this.state      = state
+        this.pageNum = 1
+        this.info    = info
+        this.state   = state
         var url = this.getNextURL(info.nextLink, document)
         if (!url) {
             return
         }
         this.requestURL = url
+        this.loadedURLs = []
         this.toggle = function() {ap.stateToggle.apply(ap)}
         this.scroll = function() {ap.onScroll.apply(ap)}
         document.body.addEventListener("dblclick", this.toggle, true)
         window.addEventListener("scroll", this.scroll, true)
-        this.insertPoint = getElementByXPath(info.insertBefore)
+        this.insertPoint = getFirstElementByXPath(info.insertBefore)
         this.initMessage('AutoPager')
     }
     AutoPager.prototype.onScroll = function() {
@@ -56,14 +93,14 @@
         this.message.style.visibility = 'hidden'
     }
     AutoPager.prototype.request = function() {
-        if (this.requested == this.requestURL) {
+        if (this.lastRequestURL == this.requestURL) {
             return
         }
-        this.requested = this.requestURL
-        this.showLoading(true)
+        this.lastRequestURL = this.requestURL
         var self = this
         var opt = {method: 'get', url: this.requestURL,
             onload: function(res){self.requestLoad.apply(self, [res])}}
+        this.showLoading(true)
         GM_xmlhttpRequest(opt)
     }
     AutoPager.prototype.showLoading = function(sw) {
@@ -78,37 +115,45 @@
     }
     AutoPager.prototype.requestLoad = function(res) {
         var t = res.responseText
-        t = t.replace(/<html.*?>/, '').replace(/<\/html>/, '')
-        var htmlDoc  = document.implementation.createDocument(
-            HTML_NAMESPACE, 'html', null)
-        var fragment = createDocumentFragmentByString(t)
-        htmlDoc.documentElement.appendChild(fragment)
-
+        var htmlDoc = createHTMLDocumentByString(t)
         var url = this.getNextURL(this.info.nextLink, htmlDoc)
-        var page = getElementByXPath(this.info.pageElement, htmlDoc)
+        var page = getFirstElementByXPath(this.info.pageElement, htmlDoc)
         if (!url || !page) {
             this.terminate()
             return
         }
+        for (var i = 0; i < this.loadedURLs.length; i++) {
+            if (this.loadedURLs[i] == this.requestURL) {
+                this.terminate()
+                return
+            }
+        }
+        this.loadedURLs.push(this.requestURL)
         this.requestURL = url
+        this.addPage(htmlDoc, page)
+        this.showLoading(false)
+    }
+    AutoPager.prototype.addPage = function(htmlDoc, page) {
         var hr = htmlDoc.createElementNS(HTML_NAMESPACE, 'hr')
-        var p  = htmlDoc.createElementNS(HTML_NAMESPACE, 'p')
-        p.appendChild(htmlDoc.createTextNode('page: ' + ++this.pageNum))
+        var p = htmlDoc.createElementNS(HTML_NAMESPACE, 'p')
+        p.appendChild(htmlDoc.createTextNode('page: ' + (++this.pageNum)))
         this.insertPoint.parentNode.insertBefore(hr, this.insertPoint)
         this.insertPoint.parentNode.insertBefore(p, this.insertPoint)
         this.insertPoint.parentNode.insertBefore(page, this.insertPoint)
-        this.showLoading(false)
     }
     AutoPager.prototype.initMessage = function(message) {
         var div = document.createElement("div")
         div.innerHTML = message
         with (div.style) {
+            fontSize   = '12px'
+            fontWeight = 'bold'
             position   = 'fixed'
-            top        = '1%'
-            right      = '1%'
+            top        = '3px'
+            right      = '3px'
             padding    = '0.3em'
             background = '#0f0'
             color      = '#fff'
+            opacity    = '0.8'
             if (this.state != 'enable') {
                 visibility = 'hidden'
             }
@@ -117,7 +162,7 @@
         this.message = div
     }
     AutoPager.prototype.getNextURL = function(xpath, doc) {
-        var next = getElementByXPath(xpath, doc)
+        var next = getFirstElementByXPath(xpath, doc)
         if (next) {
             return next.href
         }
@@ -132,55 +177,97 @@
             self.message.parentNode.removeChild(self.message)
         }, 1500)
     }
-    var HTML_NAMESPACE = 'http://www.w3.org/1999/xhtml'
 
-    // initialize.
-    var info = getInfo()
-    for (var i in info) {
-        if (location.href.match(i)) {
-            var ap = new AutoPager(info[i], 'disable')
+    var infoParser = function(str) {
+        var lines = str.split(/\r\n|\r|\n/)
+        var re = /(^[^:]*?):(.*)$/
+        var strip = function(str) {
+            return str.replace(/^\s*/, '').replace(/\s*$/, '')
+        }
+        var info = {}
+        for(var i = 0; i < lines.length; i++) {
+            if (lines[i].match(re)) {
+                info[RegExp.$1] = strip(RegExp.$2)
+            }
+        }
+        info.remainHeight = parseInt(info.remainHeight)
+        var isValid = function(info) {
+            var infoProp = ['nextLink', 'insertBefore',
+                            'pageElement', 'remainHeight']
+            for (var i = 0; i < infoProp.length; i++) {
+                if (!infoProp[i]) {
+                    return false
+                }
+            }
+            return true
+        }
+        return isValid(info) ? info : null
+    }
+
+    // initialize
+    var ap = null
+    var state = 'enable'
+    for (var i = 0; i < SITEINFO.length; i++) {
+        if (location.href.match(SITEINFO[i].url)) {
+            ap = new AutoPager(SITEINFO[i], state)
             return
         }
     }
-    return
-
-    // site data.
-    function getInfo() {
-        return {
-            'http://www.google.*/search*': {
-                nextLink:     'id("navbar")/table/tbody/tr/td[last()]/a',
-                insertBefore: 'id("navbar")',
-                pageElement:  '//div[2]',
-                remainHeight: 800
-            },
-            'http://del.icio.us/*': {
-                nextLink:     '//a[@accesskey="e"]',
-                insertBefore: 'id("bottom")',
-                pageElement:  'id("main")',
-                remainHeight: 800
-            },
-            'http://search.yahoo.co.jp/search*': {
-                nextLink:     'id("yschpg")/p/big[last()]/a',
-                insertBefore: 'id("yschpg")',
-                pageElement:  'id("yschweb")',
-                remainHeight: 800
-            },
-            'http://search.auctions.yahoo.co.jp/jp/search': {
-                nextLink:     '//td[@align="right" and @width="1%"]/small/b[last()]/a',
-                insertBefore: '//table[position()=11]',
-                pageElement:  '//table[position()=7]',
-                remainHeight: 1200
+    for (var i = 0; i < SITEINFO_IMPORT_URLS.length; i++) {
+        if (ap) {
+            return
+        }
+        var callback = function(res) {
+            if (ap) {
+                return
+            }
+            var hdoc = createHTMLDocumentByString(res.responseText)
+            var list = getElementsByXPath(
+                '//textarea[@class="autopagerize_data"]', hdoc)
+            if (list) {
+                for (var j = 0; j < list.length; j++) {
+                    var info = infoParser(list[j].value)
+                    if (info && location.href.match(info.url)) {
+                        ap = new AutoPager(info, state)
+                    }
+                }
             }
         }
+        var opt = {
+            method: 'get',
+            url: SITEINFO_IMPORT_URLS[i],
+            onload: callback,
+        }
+        GM_xmlhttpRequest(opt)
     }
+    return
 
     // utility functions.
-    function getElementByXPath(xpath, doc) {
+    function createHTMLDocumentByString(str) {
+        html = str.replace(/<!DOCTYPE.*?>/, '').replace(/<html.*?>/, '').replace(/<\/html>.*/, '')
+        var htmlDoc  = document.implementation.createDocument(null, 'html', null)
+        var fragment = createDocumentFragmentByString(html)
+        htmlDoc.documentElement.appendChild(fragment)
+        return htmlDoc
+    }
+    function getElementsByXPath(xpath, doc) {
+        var doc = doc || document
+        var iterator = doc.evaluate(xpath, doc, null,
+            XPathResult.ORDERED_NODE_ITERATOR_TYPE, null)
+        var data = []
+        var node = iterator.iterateNext()
+        while(node) {
+            data.push(node)
+            node = iterator.iterateNext()
+        }
+        return (data.length > 1) ? data : null
+    }
+    function getFirstElementByXPath(xpath, doc) {
         var doc = doc || document
         var result = doc.evaluate(xpath, doc, null,
             XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-            // for search element
-            // result.singleNodeValue.style.border = "3px solid #f00"
+        // for search element
+        // result.singleNodeValue.style.border = "3px solid #f00"
         return result.singleNodeValue ? result.singleNodeValue : null
     }
     function createDocumentFragmentByString(str) {
