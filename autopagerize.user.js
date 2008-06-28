0f5150c8e83017b4700db16c5e0f517817cdaf20
swdyh <youhei@gmail.com>
2008-06-29 03:49:53 +0900

    * 0.0.31

--- autopagerize.user.js
+++ autopagerize.user.js
@@ -8,7 +8,7 @@
 // ==/UserScript==
 //
 // auther:  swdyh http://d.hatena.ne.jp/swdyh/
-// version: 0.0.30 2008-06-28T21:52:20+09:00
+// version: 0.0.31 2008-06-29T03:49:37+09:00
 //
 // this script based on
 // GoogleAutoPager(http://la.ma.la/blog/diary_200506231749.htm) and
@@ -21,7 +21,7 @@
 
 var HTML_NAMESPACE = 'http://www.w3.org/1999/xhtml'
 var URL = 'http://userscripts.org/scripts/show/8551'
-var VERSION = '0.0.30'
+var VERSION = '0.0.31'
 var DEBUG = false
 var AUTO_START = true
 var CACHE_EXPIRE = 24 * 60 * 60 * 1000
