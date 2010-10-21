c6266686815d3572a281eba6d3de6a49216482ba
swdyh <youhei@gmail.com>
2010-10-22 01:41:09 +0900

    version 0.0.55

--- autopagerize.user.js
+++ autopagerize.user.js
@@ -11,7 +11,7 @@
 // ==/UserScript==
 //
 // auther:  swdyh http://d.hatena.ne.jp/swdyh/
-// version: 0.0.54
+// version: 0.0.55 2010-10-22T01:39:50+09:00
 //
 // this script based on
 // GoogleAutoPager(http://la.ma.la/blog/diary_200506231749.htm) and
@@ -34,7 +34,7 @@ else {
 }
 
 var URL = 'http://autopagerize.net/'
-var VERSION = '0.0.54'
+var VERSION = '0.0.55'
 var DEBUG = false
 var AUTO_START = true
 var CACHE_EXPIRE = 24 * 60 * 60 * 1000
